7e45ce86c7edf628b182603aecf2d166bdb4798b
Caio Atila P. Sena <caio.sena@inpe.br>
2024-04-23 11:40:22 -0300

    update to nbdev config

--- exp4/_modidx.py
+++ exp4/_modidx.py
@@ -5,4 +5,8 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://caioatila.github.io',
                 'git_url': 'https://github.com/caioatila/exp4',
                 'lib_path': 'exp4'},
-  'syms': {'exp4\\core': {'exp4\\core.foo': ('core.html#foo', 'exp4\\core.py')}}}
+  'syms': { 'exp4\\core': { 'exp4\\core.geo2buffer': ('core.html#geo2buffer', 'exp4\\core.py'),
+                            'exp4\\core.get_date': ('core.html#get_date', 'exp4\\core.py'),
+                            'exp4\\core.load_relato': ('core.html#load_relato', 'exp4\\core.py'),
+                            'exp4\\core.replace_val': ('core.html#replace_val', 'exp4\\core.py')},
+            'exp4\\dataset': {}}}
